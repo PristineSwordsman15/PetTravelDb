87af9a6d9823d3005fb9eaa166262c44651754f6
Atul Herle <ac122381@avcol.school.nz>
2024-06-28 12:39:43 +1200

    Made FirstLastName into FirstName and LastName

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -3,7 +3,8 @@
     public class Owner
     {
         public int OwnerID { get; set; }
-        public string FirstLastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int FlightID { get; set; }
         public int PhoneNumber { get; set;}
         public int BookingRefNo { get; set; }
